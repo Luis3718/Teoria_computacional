65f9b945e8c3cce8df7497bc3e4f70dfb7acfee6
Luis3718 <Alvarado3718@hotmail.com>
2022-02-22 20:19:30 -0600

    Complete el programa pero necesito modificar la parte de potencia

--- Tarea6.cpp
+++ Tarea6.cpp
@@ -45,12 +45,12 @@ int main ()
   cout<<"Hola cuantas palabras tiene el lenguaje 1" <<endl;
   cin>>lon1;
   fflush(stdin);
-  char l1[lon1][20];
-  char palabra[20];
+  char l1[lon1][50];
+  char palabra[50];
   for(int i=0;i<lon1;i++)
   {
     cout<<"Ingresa las palabras del lenguaje: " <<endl;
-    cin.getline(palabra,20,'\n');
+    cin.getline(palabra,50,'\n');
     fflush(stdin);
     strcpy(l1[i],palabra);
   }
@@ -67,11 +67,11 @@ int main ()
   cout<<"Hola cuantas palabras tiene el lenguaje 2" <<endl;
   cin>>lon2;
   fflush(stdin);
-  char l2[lon2][20];
+  char l2[lon2][50];
   for(int i=0;i<lon2;i++)
   {
     cout<<"Ingresa las palabras del lenguaje: " <<endl;
-    cin.getline(palabra,20,'\n');
+    cin.getline(palabra,50,'\n');
     fflush(stdin);
     strcpy(l2[i],palabra);
   }
@@ -96,7 +96,7 @@ int main ()
   {
     strcpy(l3[j+lon1],l2[j]);
   }
-  cout<<"L3= {";
+  cout<<"L1UL2= {";
   for(int i=0;i<lon1+lon2;i++)
   {
     cout<<l3[i];
@@ -106,6 +106,64 @@ int main ()
     }
   }
   cout<<"}" <<endl;
-  getch();
+  cout<<"La concatenacion de L1 con L2 es: " <<endl;
+  int r=lon1*lon2;
+  char l5[r][50];
+  char palabras[50];
+  char vacio[]="";
+  int o=0;
+  for(int i=0;i<lon1;i++)
+  {
+    for(int j=0;j<lon2;j++)
+    {
+      strcpy(palabras,l1[i]);
+      strcat(palabras,l2[j]);
+      strcpy(l5[o],palabras);
+      strcpy(palabras,vacio);
+      o++;
+    }
+  }
+  cout<<"L1*L2= {";
+  for(int i=0;i<r;i++)
+  {
+    cout<<l5[i];
+    if(i<(r)-1)
+    {
+      cout<<", ";
+    }
+  }
+  cout<<"}" <<endl;
+  int pot;
+  //hasta ahora solo sirve para potencias 2
+  cout<<"Ingrese la potencia del lenguaje" <<endl;
+  cin>>pot;
+  int res=pow(lon1,pot);
+  cout<<res <<endl;
+  char l4[pot][50];
+  //for(int i=0;i<res;i++)
+  //{
+  o=0;
+  for(int j=0;j<lon1;j++)
+  {
+    for(int k=0;k<lon1;k++)
+    {
+      strcpy(palabras,l1[j]);
+      strcat(palabras,l1[k]);
+      strcpy(l4[o],palabras);
+      strcpy(palabras,vacio);
+      o++;
+    }
+  }
+  //}
+  cout<<"L4= {";
+  for(int i=0;i<res;i++)
+  {
+    cout<<l4[i];
+    if(i<(res)-1)
+    {
+      cout<<", ";
+    }
+  }
+  cout<<"}" <<endl;
   return 0;
 }
