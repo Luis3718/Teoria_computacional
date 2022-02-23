4cffbc3c2138b712139e7c42417e403bb517be55
Luis3718 <Alvarado3718@hotmail.com>
2022-02-22 20:12:47 -0600

    Logre hacer la concatenacion

--- Tarea6.cpp
+++ Tarea6.cpp
@@ -50,7 +50,7 @@ int main ()
   for(int i=0;i<lon1;i++)
   {
     cout<<"Ingresa las palabras del lenguaje: " <<endl;
-    cin.getline(palabra,20,'\n');
+    cin.getline(palabra,50,'\n');
     fflush(stdin);
     strcpy(l1[i],palabra);
   }
@@ -67,11 +67,11 @@ int main ()
   cout<<"Hola cuantas palabras tiene el lenguaje 2" <<endl;
   cin>>lon2;
   fflush(stdin);
-  char l2[lon2][20];
+  char l2[lon2][50];
   for(int i=0;i<lon2;i++)
   {
     cout<<"Ingresa las palabras del lenguaje: " <<endl;
-    cin.getline(palabra,20,'\n');
+    cin.getline(palabra,50,'\n');
     fflush(stdin);
     strcpy(l2[i],palabra);
   }
@@ -116,7 +116,7 @@ int main ()
   {
     strcpy(l3[j+lon1],l2[j]);
   }
-  cout<<"L3= {";
+  cout<<"L1UL2= {";
   for(int i=0;i<lon1+lon2;i++)
   {
     cout<<l3[i];
@@ -127,23 +127,63 @@ int main ()
   }
   cout<<"}" <<endl;
   cout<<"La concatenacion de L1 con L2 es: " <<endl;
+  /*int pot;
+  //hasta ahora solo sirve para potencias 2
+  cout<<"Ingrese la potencia del lenguaje" <<endl;
+  cin>>pot;
+  int res=pow(lon1,pot);
+  cout<<res <<endl;
+  char l4[pot][50];
+  //for(int i=0;i<res;i++)
+  //{
+  int o=0;
+  for(int j=0;j<lon1;j++)
+  {
+    for(int k=0;k<lon1;k++)
+    {
+      strcpy(palabras,l1[j]);
+      strcat(palabras,l1[k]);
+      strcpy(l4[o],palabras);
+      strcpy(palabras,vacio);
+      o++;
+    }
+  }
+  //}
+  cout<<"L4= {";
+  for(int i=0;i<res;i++)
+  {
+    cout<<l4[i];
+    if(i<(res)-1)
+    {
+      cout<<", ";
+    }
+  }
+  cout<<"}" <<endl;*/
+  int r=lon1*lon2;
+  char l5[r][50];
+  char palabras[50];
+  char vacio[]="";
+  int o=0;
   for(int i=0;i<lon1;i++)
   {
     for(int j=0;j<lon2;j++)
     {
-      strcat(l1[j],l2[i]);
+      strcpy(palabras,l1[i]);
+      strcat(palabras,l2[j]);
+      strcpy(l5[o],palabras);
+      strcpy(palabras,vacio);
+      o++;
     }
   }
-  cout<<"L1= {";
-  for(int i=0;i<lon1;i++)
+  cout<<"L1*L2= {";
+  for(int i=0;i<r;i++)
   {
-    cout<<l1[i];
-    if(i<lon1-1)
+    cout<<l5[i];
+    if(i<(r)-1)
     {
       cout<<", ";
     }
   }
   cout<<"}" <<endl;
-  getch();
   return 0;
 }
